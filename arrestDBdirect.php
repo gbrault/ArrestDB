d09ef1a5aac1b9f1357106d6ecad244c35addb26
Gilbert Brault <gilbert.brault@hotmail.com>
2016-05-27 17:05:58 +0200

    implement SQLon

---
+++ arrestDBdirect.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+require('security.php');
+require ('Call_ArrestDB.php');
+
+if(!isset($_SESSION['user'])){
+	// if not logged exits
+	// except if caller is local
+	$ok = false;
+	
+	if(isset($_SERVER['REMOTE_ADDR']) && 
+			(($_SERVER['REMOTE_ADDR']=='::1')||($_SERVER['REMOTE_ADDR']=='127.0.0.1'))){
+		    $ok=true;
+	}
+	if (!$ok){
+		exit(ArrestDB::Reply(ArrestDB::$HTTP[302]));	
+	}
+}
+
+$dsn = 'sqlite://C:/Users/sesa33795/ArrestDB/Northwind.sqlite';
+$clients = [];
+
+if (ArrestDB::Query($dsn) === false)
+{
+	exit(ArrestDB::Reply(ArrestDB::$HTTP[503]));
+}
+
+$json = file_get_contents('php://input');
+if ((!isset($json))|| is_null($json) ){
+	$result = ArrestDB::$HTTP[204];
+	exit(ArrestDB::Reply($result));
+}
+$query = json_decode($json);
+if ((!isset($query))|| is_null($query) || ($query=="") ){
+	$result = ArrestDB::$HTTP[204];
+	exit(ArrestDB::Reply($result));
+}
+
+$result = ArrestDB::Query($query);
+
+if ($result === false)
+{
+	$result = ArrestDB::$HTTP[404];
+}
+
+else if (empty($result) === true)
+{
+	$result = ArrestDB::$HTTP[204];
+}
+
+$count = count($result);
+
+if ($count==1){
+    $colcount = count($result[0]);
+    if($colcount==1){
+		foreach($result[0] as $key => $value){
+			echo $value;
+		} 
+	} else {
+		echo ArrestDB::Reply($result);
+	}
+} else if($count>1){
+	echo ArrestDB::Reply($result);
+} else {
+	echo "{}";
+}
+
+
+
+?>
